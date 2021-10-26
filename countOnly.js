d7b58866cc7ce7ab79f68a7f987b53318bb48d9c
Alison Harman <alison_harman@sympatico.ca>
2021-10-26 21:35:42 +0000

    returns a count for a specific subset of items

---
+++ countOnly.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const countOnly = function(allItems, itemsToCount) {
+
+  const results = {};
+  // only add key:value pair to results object if included in itemsToCount object
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      //   set a property with that string key to:
+      //     the value that was already there (or zero if nothing there) with 1 added to it.
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  //console.log(results);
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
