226ff027403b41bec39ab9e1f6dc4bda3f4653b7
Alison Harman <alison_harman@sympatico.ca>
2021-10-31 01:53:30 +0000

    returns slice of array from beginning until callback equals true

---
+++ takeUntil.js
@@ -0,0 +1,43 @@
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (const item of array) {
+    if (!callback(item)) {
+      results.push(item);
+    } else {
+      return results;
+    }
+  }
+};
+// function to compare if two arrays are a perfect match
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(actual, expected) {
+  let areEqual = false; // initializing boolean
+  areEqual = eqArrays(actual, expected);
+  if (areEqual) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// TEST CASE #1
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1,2,5,7,2]);
+// TEST CASE 2
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+// TEST CASE #3
+const data3 = [3, 2, 1, -1, -2, -3];
+const results3 = takeUntil(data3, x => x > 0);
+assertArraysEqual(results3, []);
