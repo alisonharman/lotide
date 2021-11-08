00d5e6e1b3150b95a6e49efee1f6f7ca41c8f22d
Alison Harman <alison_harman@sympatico.ca>
2021-11-08 21:16:34 +0000

    updated tests to use Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,28 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-// TEST CODE
-// TEST #1
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-// TEST #2
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+
+  it('returns [2,3] for [1,2,3]', () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns [\"meow\", \"food\"] for [\"cat\",\"meow\", \"food\"]", () => {
+    assert.deepEqual(tail(["cat", "meow", "food"]), ["meow", "food"]);
+  });
+
+  it('returns empty array if given array with one element', () => {
+    assert.deepEqual(tail([1]), []);
+  });
+
+  it('returns empty array if given empty array', () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+  it('does not modify original array', () => {
+    const numbers = [2, 3, 5, 6];
+    tail(numbers);
+    assert.deepEqual(numbers, [2, 3, 5, 6]);
+  });
+
+});
