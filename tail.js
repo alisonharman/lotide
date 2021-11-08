ec85e0b100aef2af81661fe870a640706c90b462
Alison Harman <alison_harman@sympatico.ca>
2021-11-08 19:27:06 +0000

    exported tail as module

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// the tail is every element except the head (first element) of the array
 const tail = function(array) {
   if (array.length >= 1) {
     return array.slice(1);
@@ -12,16 +6,5 @@ const tail = function(array) {
     return;
   }
 };
-// TEST CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-// TEST
-const results = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(results, ["Lighthouse", "Labs"]); // => will always fail!
-// NEW TEST
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+module.exports = tail;
