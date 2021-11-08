810ab029b0347f444e0e76c4935caaf9b2328da9
Alison Harman <alison_harman@sympatico.ca>
2021-11-08 20:02:01 +0000

    refactored code to export middle as module and removed test code

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const assertArraysEqual = function(actual, expected) {
-  let areEqual = false; // initializing boolean
-  areEqual = eqArrays(actual, expected);
-  if (areEqual) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-// function to compare if two arrays are a perfect match
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 // outputs middle values of an array
 // if array has even number of elements, then output should be the two middle
 // if array has odd number of elements, then output should be one value in the middle
@@ -40,8 +19,4 @@ const middle = function(array) {
   }
   return returnedArray;
 };
-// TEST CODE
-assertArraysEqual(middle([1]), [1]); // SHOULD FAIL
-assertArraysEqual(middle([1, 2]), []); // SHOULD PASS
-assertArraysEqual(middle([1, 2, 3]), [2]); // SHOULD PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]); // SHOULD PASS
+module.exports = middle;
