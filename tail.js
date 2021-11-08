ddb9ccf8e177cd6149873f6ecdb3c983bf864c28
Alison Harman <alison_harman@sympatico.ca>
2021-11-08 21:17:07 +0000

    fixed error where empty array did not return empty array

--- tail.js
+++ tail.js
@@ -1,6 +1,12 @@
 // the tail is every element except the head (first element) of the array
 const tail = function(array) {
-  if (array.length >= 1) {
+
+  const isArray = Array.isArray(array);
+  if (!isArray) {
+    return;
+  } else if (array.length === 0) {
+    return [];
+  } else if (array.length >= 1) {
     return array.slice(1);
   } else {
     return;
