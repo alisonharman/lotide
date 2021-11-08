111e0f1fe0f60a1afe36077a918b7a40594a3b79
Alison Harman <alison_harman@sympatico.ca>
2021-11-08 19:53:03 +0000

    refactored code to export as module and require eqArrays versus hard-coded

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,5 @@
-// function to compare if two arrays are a perfect match
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(actual, expected) {
   let areEqual = false; // initializing boolean
   areEqual = eqArrays(actual, expected);
@@ -19,8 +9,4 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 3], [1, 2, '3']); // => should FAIL
-assertArraysEqual([1, 2, 3], [1, 2, -3]); // => should FAIL
-assertArraysEqual(['dog', 'boot', 'chew'], ['dog', 'boot', 'chew']); // => should PASS
+module.exports = assertArraysEqual;
