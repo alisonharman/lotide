4b8b95e331aefeaf2f0db9e0aac7eb33a7fa0242
Alison Harman <alison_harman@sympatico.ca>
2021-11-08 21:31:45 +0000

    changed test code to use Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,30 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-// TEST CODE
-assertArraysEqual(middle([1]), [1]); // SHOULD FAIL
-assertArraysEqual(middle([1, 2]), []); // SHOULD PASS
-assertArraysEqual(middle([1, 2, 3]), [2]); // SHOULD PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]); // SHOULD PASS
+describe('#middle', () => {
+
+  it('returns empty array if given array with one element (since there is no middle)', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it('returns empty array if given array with two elements (since there still is no middle)', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it('returns middle element of odd number of elements, numbered three or greater', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it('returns two middle elements of even number of elements, numbered four or greater', () => {
+    assert.deepEqual(middle([10, 12, 13, 15]), [12,13]);
+  });
+
+  it('returns undefined if not given Array input', () => {
+    assert.strictEqual(middle('1234'), undefined);
+  });
+
+  it('returns empty array if given empty array', () => {
+    assert.deepEqual(middle([]), []);
+  });
+
+});
