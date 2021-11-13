43b3e6cda34b06933761b051710f8adecdba0048
Alison Harman <alison_harman@sympatico.ca>
2021-11-13 05:42:34 +0000

    exported all functions

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const tail = require('./tail');
 const middle = require('./middle');
 const eqArrays = require('./eqArrays');
 const assertEqual = require('./assertEqual');
-const assertArraysEqual = require('./assertArraysEqual')
-
+const assertArraysEqual = require('./assertArraysEqual');
+const flatten = require('./flatten');
+const countOnly = require('./countOnly');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const eqObjects = require('./eqObjects');
+const map = require('./map');
+const countLetters = require('./countLetters');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const findKey = require('./findKey')
 
 module.exports = {
   head,
@@ -12,5 +20,14 @@ module.exports = {
   middle,
   eqArrays,
   assertEqual,
-  assertArraysEqual
+  assertArraysEqual,
+  flatten, 
+  countOnly,
+  letterPositions,
+  findKeyByValue,
+  eqObjects,
+  map,
+  countLetters,
+  assertObjectsEqual,
+  findKey
 };
