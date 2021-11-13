7e19a8dc5bf0d2b8cb41db29de787daf5b12c041
Alison Harman <alison_harman@sympatico.ca>
2021-11-13 15:14:26 +0000

    will return undefined if not given an array as first parameter or object as second

--- countOnly.js
+++ countOnly.js
@@ -1,10 +1,17 @@
-const assertEqual = require('./assertEqual');
-
+// function takes in a collection of items, i.e. an array of strings
+// AND takes in an object of which specific subset of items to count
+// items are limited to strings
 const countOnly = function(allItems, itemsToCount) {
 
+  if (!Array.isArray(allItems)) {
+    return;
+  }
   const results = {};
   // only add key:value pair to results object if included in itemsToCount object
   for (const item of allItems) {
+    if (typeof(item) !== "string") {
+      return;
+    }
     if (itemsToCount[item]) {
       //   set a property with that string key to:
       //     the value that was already there (or zero if nothing there) with 1 added to it.
@@ -19,22 +26,3 @@ const countOnly = function(allItems, itemsToCount) {
 };
 
 module.exports = countOnly;
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
