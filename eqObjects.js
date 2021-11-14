42bb673c25e6bef9735c07900be46de759431a1f
Alison Harman <alison_harman@sympatico.ca>
2021-11-14 04:54:53 +0000

    can now check is two object are equal and hold objects as value

--- eqObjects.js
+++ eqObjects.js
@@ -2,30 +2,37 @@ const eqArrays = require('./eqArrays');
 
 // objects are equal if they have the same number of keys
 // also: if value in corresponding keys is the same value as the other key
-const eqObjects = function(object1, object2) {
+const eqObjects = function (object1, object2) {
+  let returnValue = true;
   const keysFirstObject = Object.keys(object1);
   const keysSecondObject = Object.keys(object2);
 
   if (keysFirstObject.length !== keysSecondObject.length) {
-    return false;
+    returnValue = false;
   }
   for (let i = 0; i < keysFirstObject.length; i++) {
     const value1 = object1[keysFirstObject[i]];
     const value2 = object2[keysFirstObject[i]];
 
-    // need conditional to eval if the values are both arrays and apply special function to see if they are equal
-    if (Array.isArray(value1) && Array.isArray(value2)) {
-      if (!eqArrays(value1, value2)) {
-        return false;
+    if ((typeof value1 === 'object') && (typeof value2 === 'object')) {
+      if (Array.isArray(value1) && Array.isArray(value2)) {
+        if (!eqArrays(value1, value2)) {
+          returnValue = false;
+        }
       }
-      // else values are primitives and conditional with !== operator
-    } else {
+      else {
+        // they are real objects!
+        returnValue = eqObjects(value1, value2)
+      }
+    }
+    else {
+      // they are primitives
       if (value1 !== value2) {
-        return false;
+        returnValue = false;
       }
     }
   }
-  return true;
+  return returnValue;
 };
 
 module.exports = eqObjects;
